903275a15f69f694395b67975ca33a0efac568b5
Gui Ambros <nospamGitHub@ambros.com.br>
2023-12-01 01:48:17 -0500

    Code refactor

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,5 @@
-import operator
-import os
 import re
 import sys
-from collections import defaultdict
-from copy import deepcopy
-from functools import reduce
-
-from PIL import Image
 
 # get current day
 cwd = sys.argv[0]
@@ -22,10 +15,6 @@ day = int(re.search("\\/day(\\d+)", cwd).group(1))
 file_input = open(f"day{day}/input_{year}_{day}.txt", "r").read()
 # input = open(f"day{day}/input_{year}_{day}_test.txt", "r").read()
 raw_input = file_input.splitlines()
-
-# input in a single line
-# input = [line for line in input.split(",")]
-# input = [int(i) for i in input]
 
 
 def part1(input):
@@ -72,5 +61,5 @@ def part2(input):
 
 
 if __name__ == "__main__":
-    # part1(raw_input)
+    part1(raw_input)
     part2(raw_input)
