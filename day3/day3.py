ba7e749b25b209c274a55ba6d7a3d0715f8de9a1
Gui Ambros <nospamGitHub@ambros.com.br>
2023-12-03 16:09:07 -0500

    Day 2 complete

--- day3/day3.py
+++ day3/day3.py
@@ -1,11 +1,5 @@
-import math as m
-import operator
-import os
 import re
 import sys
-from collections import defaultdict
-from copy import deepcopy
-from functools import reduce
 
 # get current day
 cwd = sys.argv[0]
@@ -19,16 +13,7 @@ day = int(re.search("\\/day(\\d+)", cwd).group(1))
 
 
 input = open(f"day{day}/input_{year}_{day}.txt", "r").read()
-# input = open(f"day{day}/input_{year}_{day}_test.txt", "r").read()
-
-# input in multiple lines
 input = [line for line in input.splitlines()]
-
-# input in a single line
-# input = [line for line in input.split(",")]
-
-# convert list to int
-# input = [int(i) for i in input]
 
 
 def get_number(input, row, col):
@@ -84,12 +69,18 @@ def find_nearby_numbers(input, row, col):
         (row + 1, col + 1),
     ]
     for r, c in delta_coords:
-        if r >= 0 and r < len(input) and c >= 0 and c < len(input[r]):
+        if (
+            0 <= r < len(input)
+            and 0 <= c < len(input[r])
+            and (r, c) not in coords_processed
+            and input[r][c].isdigit()
+        ):
             ch = input[r][c]
             if ch.isdigit() and (r, c) not in coords_processed:
                 num, coords = get_number(input, r, c)
                 coords_processed += coords
                 numbers.append(num)
+
     return numbers
 
 
@@ -128,37 +119,6 @@ def part2(input):
     print(f"Part 2: total sum of gears multiplied {sum(gears)}")
 
 
-def solution(input):
-    board = list(input)
-    chars = {
-        (r, c): [] for r in range(140) for c in range(140) if board[r][c] not in "01234566789."
-    }
-
-    for r, row in enumerate(board):
-        for n in re.finditer(r"\d+", row):
-            edge = {(r, c) for r in (r - 1, r, r + 1) for c in range(n.start() - 1, n.end() + 1)}
-
-            for o in edge & chars.keys():
-                chars[o].append(int(n.group()))
-
-    print(
-        sum(sum(p) for p in chars.values()), sum(m.prod(p) for p in chars.values() if len(p) == 2)
-    )
-
-
 if __name__ == "__main__":
     part1(input)
     part2(input)
-
-# Example of an input that requires regex parsing
-# Format:
-#   kvlbq (22)
-#   rdrad (6) -> gwyfm, fozyip, uotzz, fmkkz
-#   oqbfkud (470) -> rnbqhk, mepez, mnksdxf, mjsck, bbfaxid, nglea
-#   zzjyw (91)
-#
-# t = dict( \
-#    (m[0], (int(m[1]), m[3].split(", ") if m[3] else [])) \
-#       for m in [re.match("(\w+) \((\d+)\)( -> ((\w+, )*\w+))?", l).groups() \
-#       for l in d] \
-# )
