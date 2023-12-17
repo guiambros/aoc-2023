8bcee9af81756e435a9c4b194aa9db9a62762dd0
Gui Ambros <nospamGitHub@ambros.com.br>
2023-12-16 23:32:28 -0500

    Day 12 pt2 complete, usign recursion

--- day12/day12.py
+++ day12/day12.py
@@ -1,11 +1,7 @@
-import operator
-import os
 import re
 import sys
 import time
-from collections import defaultdict
-from copy import deepcopy
-from functools import cache, lru_cache, reduce
+from functools import cache
 from multiprocessing import Pool
 
 # get current day
@@ -59,7 +55,7 @@ def fuzz(s, size):
     return combinations
 
 
-def cnt_valid_combinations(s, breakdown):
+def num_valid_solutions_bruteforce(s, breakdown):
     combinations = fuzz(s, len(s))
     valid_combinations = 0
     for c in combinations:
@@ -68,31 +64,98 @@ def cnt_valid_combinations(s, breakdown):
     return valid_combinations
 
 
-def solve_multip(i):
+def solve_bruteforce(i):
     (s, breakdown) = M[i]
     breakdown = tuple([int(b) for b in breakdown.split(",")])
-    valid_combinations = cnt_valid_combinations(s, breakdown)
-    print(f"{i}: {s} ({breakdown}) = {valid_combinations}")
+    valid_combinations = num_valid_solutions_bruteforce(s, breakdown)
+    # print(f"{i}: {s} ({breakdown}) = {valid_combinations}")
     return valid_combinations
 
 
-def part1(input):
+@cache
+def num_valid_solutions(s, groups):
+    # inspired by reddit comment by u/damaltor1 and @xavdid
+    # https://reddit.com/r/adventofcode/comments/18ghux0/2023_day_12_no_idea_how_to_start_with_this_puzzle/kd0npmi/
+    # https://advent-of-code.xavd.id/writeups/2023/day/12/
+
+    # if we got to the end of the line and no more blocks to check
+    # return 1 if we have a valid solution, 0 otherwise
+    if not s:
+        return 1 if len(groups) == 0 else 0
+
+    # if we still have blocks to check, make sure there's no more '#'
+    if not groups:
+        return 1 if "#" not in s else 0
+
+    # if it starts with a ., discard the . and recursively check again.
+    if s[0] == ".":
+        return num_valid_solutions(s[1:], groups)
+
+    # if it starts with a ?, replace the ? with a . and recursively check again, AND replace it with a # and recursively check again.
+    if s[0] == "?":
+        return num_valid_solutions("#" + s[1:], groups) + num_valid_solutions("." + s[1:], groups)
+
+    # it it starts with a #, check if it is long enough for the first group, check if all characters in the first [grouplength]
+    # characters are not '.', and then remove the first [grouplength] chars and the first group number, recursively check again.
+    if s[0] == "#":
+        if (
+            len(s) < groups[0]  # not enough characters left
+            or "." in s[: groups[0]]  # . breaks the sequence
+            or (len(s) > groups[0] and s[groups[0]] == "#")  # group would be too big
+        ):
+            return 0
+
+        return num_valid_solutions(s[groups[0] + 1 :], groups[1:])
+    raise Exception("Invalid character {s[0]}")
+
+
+def solve(i, p2=False):
+    (s, breakdown) = M[i]
+    if p2:
+        s = ((s + "?") * 5)[:-1]
+        breakdown = ((breakdown + ",") * 5).strip(",")
+    breakdown = tuple([int(b) for b in breakdown.split(",")])
+    valid_combinations = num_valid_solutions(s, breakdown)
+    # print(f"{i}: {s} ({breakdown}) = {valid_combinations}")
+    return valid_combinations
+
+
+def solve_pt2(i):
+    return solve(i, p2=True)
+
+
+def part1_bruteforce():
+    total = sum(map(solve_bruteforce, range(len(M))))
+    print(f"Part 1 (brute force, no parallel processing): {total}")
+
+
+def part1():
     total = 0
-    with Pool(processes=12) as p:
-        total += sum(p.map(solve_multip, range(len(M))))
-    print(f"Part 1: {total}")
+    with Pool() as p:
+        total += sum(p.map(solve, range(len(M))))
+    print(f"Part 1 (recursive with dp, parallel processing): {total}")
 
 
-def part2(input):
-    print(f"Part 2: {None}")
-    pass
+def part2():
+    total = 0
+    with Pool() as p:
+        total += sum(p.map(solve_pt2, range(len(M))))
+    print(f"Part 2: {total}")
 
 
 if __name__ == "__main__":
+    # part1 via brute force
+    # start_time = time.time()
+    # part1_bruteforce()
+    # print(f"Execution time: {time.time() - start_time}\n")
+
+    # part1 via dp and parallel processing
     start_time = time.time()
-    part1(input)
+    part1()
     print(f"Execution time: {time.time() - start_time}\n")
 
+    # part2 via recursion
     start_time = time.time()
-    part2(input)
+    part2()
     print(f"Execution time: {time.time() - start_time}\n")
+    pass
