53a8e07255dbc4649f6098a5188b7b9b6e882134
Gui Ambros <nospamGitHub@ambros.com.br>
2023-12-17 23:01:10 -0500

    Day 14 part 2 complete

--- day14/day14.py
+++ day14/day14.py
@@ -1,11 +1,21 @@
 import hashlib
-import operator
-import os
 import re
 import sys
-from collections import defaultdict
+import time
 from copy import deepcopy
 from functools import cache, lru_cache, reduce
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function {func.__name__} took {end_time - start_time} seconds to run.")
+        return result
+
+    return wrapper
+
 
 # get current day
 cwd = sys.argv[0]
@@ -30,59 +40,12 @@ def rotate_map_counterclockwise(m):
     return list(reversed([list(i) for i in zip(*m)]))
 
 
-def tilt_right(m):
-    h = len(m)
-    w = len(m[0])
-
-    def move_right(r, c):
-        cnt = 0
-        max_right = c
-        for i in range(c + 1, w):
-            if m[r][i] == ".":
-                max_right = i
-                cnt += 1
-            elif m[r][i] in ["#", "O"]:
-                break
-            else:
-                print("error!")
-                break
-        if cnt:
-            m[r][c] = "."
-            m[r][max_right] = "O"
-            return (r, max_right)
-        return None
-
-    for r in range(h):
-        for c in range(w - 1, -1, -1):
-            if m[r][c] == "O":
-                while True:
-                    res = move_right(r, c)
-                    if res is None:
-                        break
-                    (r, c) = res
-    return m
-
-
 def calculate_score(m):
     h = len(m)
     w = len(m[0])
     return sum([(h - r) for r in range(h) for c in range(w) if m[r][c] == "O"])
 
 
-def part1(M):
-    # M = rotate_map_clockwise(M)
-    # tilt_right(M)
-    # M = rotate_map_counterclockwise(M)
-    # score = calculate_score(M)
-    # print(f"Part 1: {score}")
-    # pass
-    tilt(M)
-    score = calculate_score(M)
-    print(f"Part 1: {score}")
-    # pass
-
-
-@cache
 def tilt(M):
     h = len(M)
     w = len(M[0])
@@ -99,52 +62,37 @@ def tilt(M):
     return M
 
 
-def tilt_cycle_orig(M, n=1):
-    hash_cache = {}
-    cycle_cache = {}
-    cycle_start, cycle_end = float("inf"), float("-inf")
-    cycle = None
-
-    for i in range(4 * n):
-        h = hashlib.sha256(str(M).encode()).hexdigest()
-        if h in hash_cache:
-            print(f"Iteration {i} matches the one seen at {hash_cache[h][0]}")
-            cycle, M = hash_cache[h]
-            cycle_start = min(cycle_start, cycle)
-            cycle_end = max(cycle_end, cycle)
-            cycle_cache[cycle - cycle_start] = deepcopy(M)
-            if cycle < cycle_end:  # end of the cycle
-                print(f"Cycle length is {cycle_end - cycle_start}")
-                print(f"Cycle starts at {cycle_start} and ends at {cycle_end}")
-                break
-        else:
-            M = rotate_map_clockwise(M)
-            M = tilt_right(M)
-            hash_cache[h] = (i, deepcopy(M))
-
-        if i % 10000 == 0:
-            print(f"tilt cycle {i}")
-    final_cycle = cycle_cache[(n - cycle) % (cycle_end - cycle_start)] if cycle else M
-    # M = rotate_map_counterclockwise(final_cycle)
-    return M
-
-
 def tilt_cycle(M, n=1):
-    cache = {}
-    cycle_cache = {}
-    cycle_start, cycle_end = float("inf"), float("-inf")
-    cycle = None
-    for _ in range(n):
+    i, M_cache = 0, {}
+    cache_ptr, cycle_start, cycle_end = 0, float("inf"), float("-inf")
+    cycle_detection = True
+
+    while i < n:
         h = hashlib.sha256(str(M).encode()).hexdigest()
-        if h in cache:
-            # do something with the cycle
-            pass
+        if h in M_cache:
+            ptr = M_cache[h][0]
+            # print(f"\nIteration {i} matches the one seen at {ptr} -- {h}")
+            cycle_start = min(cycle_start, ptr)
+            cycle_end = max(cycle_end, ptr)
+            cycle_length = cycle_end - cycle_start + 1
+            if cycle_detection and ptr == cycle_start and cycle_end > cycle_start:
+                print(
+                    f"Found pattern; cycle starts at {cycle_start}, ends at {cycle_end}, length {cycle_length}"
+                )
+                cycle_detection = False
+                i += ((n - i) // cycle_length) * cycle_length - 1  # skip to the end of the cycle
+            else:
+                M = M_cache[h][1]
         else:
-            cache[h] = deepcopy(M)
+            M_input = deepcopy(M)
             for _ in range(4):
                 M = tilt(M)
                 M = rotate_map_clockwise(M)
-                h = hashlib.sha256(str(M).encode()).hexdigest()
+            h = hashlib.sha256(str(M_input).encode()).hexdigest()
+            M_cache[h] = (cache_ptr, deepcopy(M))  # cache ptr, input, output
+            cache_ptr += 1
+        # print(f"After rotation {i}: {hashlib.sha256(str(M).encode()).hexdigest()}")
+        i += 1
 
     return M
 
@@ -155,14 +103,18 @@ def print_map(M):
         print("".join(row))
 
 
+@timer
+def part1(M):
+    tilt(M)
+    score = calculate_score(M)
+    print(f"Part 1: {score}")
+
+
+@timer
 def part2(M):
     M = tilt_cycle(M, 1000000000)
-    # M = tilt_cycle(M, 3)
     score = calculate_score(M)
-    # print(f"Map:\n{M}\n\n")
-    # print(["".join(row) for row in M])
     print(f"Part 2: {score}")
-    pass
 
 
 M = [list(line) for line in input]
